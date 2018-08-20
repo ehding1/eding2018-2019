79927a82a2e5f881e60625c8c53ec05631dae293
APCS3rd <APCS3rd@MHS-Dreyer-07>
2018-08-20 10:43:30 -0700

    First commit to Github

---
+++ Hello First Coding Project/src/Hello_World.java
@@ -0,0 +1,16 @@
+
+/**
+ *Print message in console.
+ * 
+ * @author Eric Ding 
+ * @version 8/17/2018
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Eric Ding"); //Display the string
+    }
+}
+
+
