e6f3b133c07d67f14fc27893d16008f951204960
APCS3rd <APCS3rd@MHS-Dreyer-07>
2018-09-04 09:48:57 -0700

    Pikachu Code

---
+++ Pikachu Code/src/Pikachu.java
@@ -0,0 +1,14 @@
+/*this will print out Pikachu
+ * author Eric Ding
+ * version 8/31/18 
+ */
+public class Pikachu {
+
+	public static void main(String[] args) {
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t  (\\__/)");
+		System.out.println("\t\t  (o^.^)");
+		System.out.println("\t\tz(_(\")(\")");
+	}
+
+}
